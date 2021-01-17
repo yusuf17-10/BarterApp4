846529d82bc36f1a45de8a2c4aa2803395a9324b
yusuf <vadapallimumtaj123@gmail.com>
2021-01-17 13:44:14 +0530

    barter4

--- Screens/HomeScreen.js
+++ Screens/HomeScreen.js
@@ -1,32 +1,29 @@
 import * as React from "react";
 import {Text,TouchableOpacity,View} from "react-native";
 import { FlatList } from "react-native-gesture-handler";
-
+import db from "../config";
+import firebase from "firebase";
 
 export default class HomeScreen extends React.Component{
     constructor(){
         super();
         this.state={
-            allRequests:""
+            allRequests:[]
         }
+        this.requestRef=null
     }
-    render(){
-        return(
-            <View>
-                <Text style={{color:"orange",alignSelf:"center",fontSize:25,marginBottom:10}}>
-                    Home Screen
-                </Text>
 
-            <FlatList 
-
-                keyExtractor={this.keyExtractor}
-                data={this.state.allRequests}
-                renderItem={this.renderItem}
-            />
-
-            </View>
-        )
+    getObjectList=()=>{
+            this.requestRef = db.collection("allRequests")
+            .onSnapshot((snapshot)=>{
+              var allRequests = snapshot.docs.map(document => document.data());
+              this.setState({
+                allRequests : allRequests
+              });
+            })
+          
     }
+
 
     renderItem = ({item,i}) =>{
         console.log(item.item_name);
@@ -46,5 +43,35 @@ export default class HomeScreen extends React.Component{
             />
         )
     }
+
+    keyExtractor=(item,index)=>index.toString()
+
+    componentDidMount(){
+        this.getObjectList();
+    }
+    
+    componentWillUnmount(){
+        this.requestRef()
+    }
+
+    render(){
+        return(
+            <View>
+                <Text style={{color:"orange",alignSelf:"center",fontSize:25,marginBottom:10}}>
+                    Home Screen
+                </Text>
+
+            <FlatList 
+
+                keyExtractor={this.keyExtractor}
+                data={this.state.allRequests}
+                renderItem={this.renderItem}
+            />
+
+
+            </View>
+        )
+    }
+
 
 }
